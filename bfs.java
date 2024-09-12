f1178211b3e29fc886e439e9cc5d6bfe3654956b
artherbhion <artherbhion2004@gmail.com>
2024-09-12 12:13:56 +0530

    Added new line

--- bfs.java
+++ bfs.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class bfs {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-
+//Added new line
         s.close();
     }
 }
