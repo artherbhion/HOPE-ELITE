60b7db8c6731900dab2711172823ce9dfabbb292
artherbhion <artherbhion2004@gmail.com>
2024-09-12 13:37:49 +0530

    test

--- bfs.java
+++ bfs.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class bfs {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-//Added new line
-int n=0;
+        int n=0;
+        System.out.println(n);
         s.close();
     }
 }
