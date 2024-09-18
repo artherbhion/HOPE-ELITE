216766b49afa0398fcc488bf833faf652c407fd1
artherbhion <147584933+artherbhion@users.noreply.github.com>
2024-09-18 16:24:54 +0530

    sorted sq

--- sortedsquare.c
+++ sortedsquare.c
@@ -2,12 +2,13 @@
 #include<stdlib.h>
 int main(){
     int n;
-    printf("Enter no of Elements :");
     scanf("%d",&n);
     int arr[n];
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
+    for(int)
+
     for(int i=0;i<n;i++){
         printf("%d ",arr[i]);
     }
